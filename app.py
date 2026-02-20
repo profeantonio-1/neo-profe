e6c33600f8c8ed80ef8e990965e9c150df44bf5e
profeantonio-1 <antoniomanuellopezmarti@gmail.com>
2026-02-20 09:27:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ if check_password():
 
         # Mantenemos el modelo que funciona bien
         model = genai.GenerativeModel(
-            'gemini-2.0-flash',
+            'gemini-flash-latest',
             system_instruction=instrucciones_neo 
         )
     except Exception:
